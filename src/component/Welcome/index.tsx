d6d177f5a5b62cebd1eab71f4ebdcc3bb636a1cb
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-10 10:23:37 +0500

    !feat(loadash) : lodash added

--- src/component/Welcome/index.tsx
+++ src/component/Welcome/index.tsx
@@ -1,6 +1,18 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import debounce from 'lodash'
 
-const WelcomeComponent : FC <{}> = () => (
-  <h1>Welcome </h1>
-)
+const WelcomeComponent : FC <{}> = () => {
+  const [showField, setShowField] = useState<boolean>(false);
+  const handleShowField = () => {
+    setShowField(!showField)
+  }
+  return (
+    <div>
+      <button type="button" onClick={handleShowField}>
+        {' '}
+        {showField ? 'Hide Field' : 'Show Field'}
+      </button>
+    </div>
+  )
+}
 export default WelcomeComponent;
