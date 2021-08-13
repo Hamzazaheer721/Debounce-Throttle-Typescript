b7a63b16ab3d6aa96fba545a4af6ae6a9e82f9ad
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-13 10:55:54 +0500

    !fix(indent): fixed the line spaces

--- src/component/Welcome/index.tsx
+++ src/component/Welcome/index.tsx
@@ -5,9 +5,11 @@ import { Container, Button } from './index.styled';
 
 const WelcomeComponent : FC <{}> = () => {
   const [showField, setShowField] = useState<boolean>(false);
+
   const handleShowField = debounce(() => {
     setShowField(!showField)
   }, 1000) // adding delay of 1 second
+
   return (
     <Container>
       <Button type="button" onClick={handleShowField}>
