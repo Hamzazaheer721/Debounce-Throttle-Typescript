b5cf797ae0f6f62c9ea8cfdc5f2ce3c1146d3566
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-13 10:54:42 +0500

    !fix(throttle): throttle has been added to the repo

--- src/component/inputField/index.styled.ts
+++ src/component/inputField/index.styled.ts
@@ -3,21 +3,21 @@ import styled from 'styled-components'
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+    margin-top: 10px;
 `
 
 export const MiniContainer = styled.div`
     display: flex;
     align-items: center;
-    justify-content: space-between;
 `
 export const HighlightedText = styled.h5`
     font-weight: bold;
+    margin: 0 20px 0 20px;
 `
 export const InputField = styled.input`
-    width: 120px;
+    width: 500px;
     border-radius: 5px;
     outline: none;
 `
-export const Text = styled.h5`
-
+export const Text = styled.p`
 `
