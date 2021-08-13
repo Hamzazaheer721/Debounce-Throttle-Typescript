c6787e3b1bc1e68d31712e6732bc56178fa58e29
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-13 10:43:25 +0500

    !fix(throttle): added throttle to input

--- src/component/inputField/index.tsx
+++ src/component/inputField/index.tsx
@@ -1,17 +1,37 @@
-import { debounce } from 'lodash';
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
+import debounce from 'lodash/debounce';
+import throttle from 'lodash/throttle'
+import {
+  Container, HighlightedText, MiniContainer, Text,
+} from './index.styled';
 
 const InputFieldComponent : FC <{}> = () => {
-  const [, setValue] = useState<string>('')
+  const [debouncedValue, setDebouncedValue] = useState<string>('')
+  const [throttledValue, setThrottledValue] = useState<string>('');
 
-  const handleChange = debounce((e) => {
-    setValue(e.target.value)
+  const applyDebounce = debounce((_value: string) => {
+    setDebouncedValue(_value)
   }, 1000)
 
+  const applyThrottle = throttle((_value: string) => {
+    setThrottledValue(_value)
+  }, 1000)
+  const handleChange = useCallback((e : any) => {
+    applyDebounce(e.target.value);
+    applyThrottle(e.target.value);
+  }, [])
   return (
-    <>
+    <Container>
       <input type="text" placeholder="type something here ..." onChange={handleChange} />
-    </>
+      <MiniContainer>
+        <HighlightedText>Debounce : </HighlightedText>
+        <Text>{debouncedValue}</Text>
+      </MiniContainer>
+      <MiniContainer>
+        <HighlightedText>Throttle :</HighlightedText>
+        <Text>{throttledValue}</Text>
+      </MiniContainer>
+    </Container>
   )
 }
 
