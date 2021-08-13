54c99ea0e093cc2ddd2e84c3e32b98a31f833eac
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-13 15:04:38 +0500

    !fix(memo): memoizing the components

--- src/routes/Welcome/index.tsx
+++ src/routes/Welcome/index.tsx
@@ -1,12 +1,14 @@
-import { FC, lazy, Suspense } from 'react';
+import {
+  FC, lazy, memo, Suspense,
+} from 'react';
 import Loader from '../../component/Loader';
 
 const LazyView = lazy(() => import('./view'));
 
-const Welcome : FC<{}> = (props) => (
+const Welcome : FC<{}> = memo((props) => (
   <Suspense fallback={Loader}>
     <LazyView {...props} />
   </Suspense>
-)
+));
 
 export default Welcome;
